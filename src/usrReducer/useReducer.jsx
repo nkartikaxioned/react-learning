b10e683cef204a5d18c18cc29632649df21f0026
nkartikaxioned <kartikn@axioned.com>
2025-07-20 20:11:23 +0530

    fix: useReducer with switch case and data as object

--- src/usrReducer/useReducer.jsx
+++ src/usrReducer/useReducer.jsx
@@ -1,26 +1,53 @@
-import { useReducer } from "react"
-
+import { useReducer } from "react";
 
 export const UseReducer = () => {
+  const initialState = {
+    count: 0,
+    name: "",
+  };
 
   const reducer = (state, action) => {
-    if(action.type === "increment") {
-      return state + 1;
-    }
-    if(action.type === "decrement" && state > 0) {
-      return state - 1;
-    } else {
-      return state;
-    }
-  }
+    // if(action.type === "increment") {
+    //   return state + 1;
+    // }
+    // if(action.type === "decrement" && state > 0) {
+    //   return state - 1;
+    // } else {
+    //   return state;
+    // }
 
-  const [count, dispatch] = useReducer(reducer, 0);
+    switch (action.type) {
+      case "increment":
+        return {
+          ...state,
+          count: state.count + 1,
+        };
+
+      case "decrement":
+        return {
+          ...state,
+          count: state.count - 1,
+        };
+
+      case "reset":
+        return {
+          ...state,
+          count: 0,
+        };
+
+      default:
+        break;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div>
-      <p>count: {count}</p>
-      <button onClick={() => dispatch({type: "decrement"})}>Decrement</button>
-      <button onClick={() => dispatch({type: "increment"})}>Increment</button>
+      <p>count: {state.count}</p>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
     </div>
-  )
-}
+  );
+};
